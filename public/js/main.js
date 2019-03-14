2eacd48f2912241de0e3eb865d8a6595abd98200
Lukas Mueller <lukas1@umbc.edu>
2019-03-14 09:12:45 -0400

    Removed functionality for non-AB algorithms because they're irrelevant to this project. start() in main.js instructs you how to start a game between two computer players. Added parameters to playGame() in main.js for eval methods for each player. Each player can use different eval methods (support for eval_1 and eval_2 at this point).

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,20 @@
 var start = function(){
 
-  console.log('Enter info as follows:\nplayGame(algo=4, skillW=(1,2,3), skillB=(1,2,3))');
+  console.log('Enter info as follows:\nplayGame(algo=4, skillW=(1,2,3), skillB=(1,2,3), evalW=(1,2), evalB=(1,2))');
   return;
 }
 
 // Computer makes a move with algorithm choice and skill/depth level
-// TO-DO: NEED PARAMETER FOR EVAL METHOD (1, 2, or 3)
-var makeMove = function(algo, skill=1) {
+var makeMove = function(skill=1, eval=1) {
   // exit if the game is over
   if (game.game_over() === true) {
     console.log('game over');
     return;
   }
-  // Calculate the best move, using chosen algorithm
-  if (algo === 1) {
-    var move = randomMove();
-  } else if (algo === 2) {
-    var move = calcBestMoveOne(game.turn());
-  } else if (algo === 3) {
-    var move = calcBestMoveNoAB(skill, game, game.turn())[1];
-  } else { // algo = 4 (alpha-beta pruning) - ONLY ALGO USED FOR PROJECT
-    var move = calcBestMove(skill, game, game.turn())[1];
-  }
+  
+  // alpha-beta pruning
+  var move = calcBestMove(skill, game, game.turn(), eval)[1];
+  
   // Make the calculated move
   game.move(move);
   // Update board positions
@@ -29,21 +22,16 @@ var makeMove = function(algo, skill=1) {
 }
 
 // Computer vs Computer
-// Always have algo=4 for alpha-beta pruning
-// skillW is the depth White goes to, skillB is the depth Black goes to
-// TO-DO: ADD 2 MORE PARAMETERS; 1 FOR WHITE'S CHOSEN EVAL, 1 FOR BLACK'S (COULD BE 1, 2, OR 3)
-var playGame = function(algo=4, skillW=1, skillB=1, evalW=1, evalB=1) {
+var playGame = function(skillW=1, skillB=1, evalW=1, evalB=1) {
   if (game.game_over() === true) {
     console.log('game over');
     return;
   }
-  
   var skill = game.turn() === 'w' ? skillW : skillB;
   var eval = game.turn() === 'w' ? evalW : evalB;
-
-  makeMove(algo, skill, eval);
+  makeMove(skill, eval);
   window.setTimeout(function() {
-    playGame(algo, skillW, skillB, evalW, evalB); //recursion
+    playGame(skillW, skillB, evalW, evalB);
   }, 250);
 };
 
